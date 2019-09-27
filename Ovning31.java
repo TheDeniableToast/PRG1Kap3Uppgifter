d896d9208cdb8eae8f65c7c78ab4605542b6ef5f
Filip Norberg <filip.norberg@learnet.se>
2019-09-27 11:18:15 +0200

    added files 27-09-19

---
+++ Ovning31.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+import java.util.Scanner;
+
+public class Ovning31 {
+	public static void main (String[] arg) {
+		int i = 20;
+		byte d = 3;
+		i = 5.007;
+		d = 100;
+		int b = i*d;
+		System.out.println(b);
+	}
+}
+		
---
+++ Ovning32.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+import java.util.Scanner;
+
+public class Ovning32 {
+	public static void main (String[] arg) {
+		double i = 20;
+		byte d = 3;
+		i = 5.007;
+		d = 100;
+		double b = i*d;
+		System.out.println(b);
+	}
+}
+		
